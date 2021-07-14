dac34d646633a2cf543e9edbf5a998484a38501c
RezaTrist <rezatristana26@gmail.com>
2021-07-14 12:39:22 +0700

    Buat WelcomePage

---
+++ lib/views/welcome_page.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Warehouse App',
+              style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            SizedBox(
+              width: 250,
+              height: 250,
+              child: Image.asset('assets/images/warehouse.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 35,
+                right: 35,
+                bottom: 100,
+              ),
+              child: Text(
+                'This application is a place for warehouses to store their goods data.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color.fromRGBO(183, 183, 183, 1),
+                ),
+              ),
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      right: 2,
+                      left: 10,
+                    ),
+                    child: new Container(
+                      alignment: Alignment.center,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            fixedSize: Size(163, 46),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            primary: Color.fromRGBO(35, 42, 255, 1)),
+                        onPressed: () {},
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      right: 10,
+                      left: 2,
+                    ),
+                    child: new Container(
+                      alignment: Alignment.center,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: Size(163, 46),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            side: BorderSide(
+                              color: Color.fromRGBO(255, 0, 0, 1),
+                              width: 2,
+                            ),
+                          ),
+                          primary: Colors.white,
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color.fromRGBO(255, 0, 0, 1),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
