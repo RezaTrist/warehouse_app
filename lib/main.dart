a1df662549ef6015e370d0221038a7c9d9f84cfd
Reza Tristana <rezatristana26@gmail.com>
2021-08-16 16:27:07 +0700

    bug

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:warehouse_app/repo/repositories/product_repo/add_product_repository.dart';
+import 'package:warehouse_app/repo/repositories/product_repo/upd_product_repository.dart';
 import 'package:warehouse_app/repo/repositories/user_repo/firebase_auth_repository.dart';
 import 'package:warehouse_app/repo/repositories/user_repo/register_api_repository.dart';
 
@@ -17,9 +18,12 @@ void main() async {
   final FirebaseAuthRepo _firebaseAuthRepo = FirebaseAuthRepo();
   final RegisterApiRepository _registerApiRepository = RegisterApiRepository();
   final AddProductRepository _addProductRepository = AddProductRepository();
+  final UpdateProductRepository _updateProductRepository =
+      UpdateProductRepository();
   runApp(MyApp(
     firebaseAuthRepo: _firebaseAuthRepo,
     registerApiRepository: _registerApiRepository,
     addProductRepository: _addProductRepository,
+    updateProductRepository: _updateProductRepository,
   ));
 }
